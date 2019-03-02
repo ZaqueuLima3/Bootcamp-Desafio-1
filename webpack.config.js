3b53bede7970000a77afe923427af7c70d6e7d42
ZAQUEU LIMA DOS SANTOS <zaqueulima1@gmail.com>
2019-03-02 14:19:14 -0300

    Storing post in state

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'components', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
